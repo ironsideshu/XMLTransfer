acb401b3c3e6cc25d9c1f46e7a47cb7397517a7d
Steven Hu <shu@ironsidegroup.com>
2015-04-29 11:31:20 -0400

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'transfer/xml'
 
+  root :to => redirect('transfer/csv')
+
+  get 'transfer/xml'
   get 'transfer/csv'
+
+  resources :transfer
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
