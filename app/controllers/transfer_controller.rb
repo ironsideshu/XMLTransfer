fbaf6ef8c2305cebec4f09be5e96b7be6b5830d4
Steven Hu <shu@ironsidegroup.com>
2015-04-29 11:36:14 -0400

    testing xml page blank

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -2,14 +2,15 @@ class TransferController < ApplicationController
 	respond_to :html, :xml
 
 	def xml
-		url = URI.parse('https://www.ncdc.noaa.gov/swdiws/xml/warn/20150401:20151231')
-		http = Net::HTTP.new(url.host, url.port)
-		http.use_ssl = true
+		
+		#url = URI.parse('https://www.ncdc.noaa.gov/swdiws/xml/warn/20150401:20151231')
+		#http = Net::HTTP.new(url.host, url.port)
+		#http.use_ssl = true
 
-		req = Net::HTTP::Get.new(url.request_uri)
-		req["token"] = "nPQjQSSBMYMlcNvdpEuVQnabTXXBKfcC"
-		@res = Net::HTTP.start(url.host, url.port) { http.request(req) }
-		render xml: @res.body
+		#req = Net::HTTP::Get.new(url.request_uri)
+		#req["token"] = "nPQjQSSBMYMlcNvdpEuVQnabTXXBKfcC"
+		#@res = Net::HTTP.start(url.host, url.port) { http.request(req) }
+		#render xml: @res.body
 	end
 
 	def csv
